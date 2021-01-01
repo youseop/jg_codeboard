b5e1e328bdc9a52dd3e786be0a314144cee5bd86
kdu133 <kdu133@hanmail.net>
2021-01-01 17:45:52 +0900

    search&sort-1

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from ..models import Question
+from django.db.models import Q, Count
 
 def index(request):
+
     page = request.GET.get('page', '1')
-    question_list = Question.objects.order_by('-create_date')
+    kw = request.GET.get('kw', '')
+    so = request.GET.get('so', 'recent')
+
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_voter=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
